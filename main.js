916caeb4b973c710f22ab885c665ea32f2d032b0
DiOlllIV <denisolekh@gmail.com>
2020-01-25 23:39:47 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+//#2 Функция возврата минимального элемента в массиве
+let arr = [2, 65, 76, 34, 23, 89, 4, 8, 6, 54];
+
+function getMin(arr)
+{
+    return [ Math.min.apply(Math, arr) ];
+
+}
+alert(getMin(arr) );
+ 
+//#3 Четные в массиве
+getEven = arr.filter(n => n % 2 !== 1);
+alert(getEven);
+
+//#4
+let users =
+[
+    {name: 'Jonny Walker', birthDate: '1995-12-17'},
+    {name: 'Andrew', birthDate: '2001-10-29'},
+    {name: 'Victor', birthDate: '1998-11-09'},
+]
+
+let user = prompt("Enter a name: ", 'user');
+for(i = 0; i > users.legth; i++)
+{
+    user = users.find(item => item.name == users[i]);
+}    
+console.log(users[i]);
